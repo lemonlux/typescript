60307030680ec5b8f9cf3c45a5b5488bb10f3141
lemonlux <sololucia17@gmail.com>
2024-01-16 16:51:17 +0100

    depuracion

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let mensaje = "hola que tal";
+console.log(mensaje, typeof mensaje);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let mensaje: string = "hola que tal"
+
+mensaje = "despues del breakpoint"
+
+console.log(mensaje, typeof mensaje)
+
+mensaje= "mensaje final"

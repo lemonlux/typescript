3b713a882ba76d8c27d6e5db4b8a1bfd640cf0d4
lemonlux <sololucia17@gmail.com>
2024-01-17 14:51:31 +0100

    types

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,45 @@
 "use strict";
 let mensaje = "hola que tal";
+mensaje = "despues del breakpoint";
 console.log(mensaje, typeof mensaje);
+mensaje = "mensaje final";
+const arr = ["Array", "Hola"];
+console.log(arr, typeof arr);
+let age = 27;
+let youngAdult = true;
+const myName = "Lucía Martínez Cerón";
+console.log(typeof myName);
+const suma = (a, b) => {
+    return a + b;
+};
+console.log(suma(4, 5));
+let animal = ["caballito", "de", "mar"];
+let num = [33, 44, 55];
+let animales = ["caballito", "de", "mar"];
+let nums = [33, 44, 55];
+let num2 = [];
+num2 = [44, 55];
+console.log(typeof num, typeof num2);
+let nameAndAge = ["Lucía", 27];
+let fullNameAndAge = [["Lucía", "Martínez", "Cerón"], 27];
+nameAndAge.push(12);
+console.log(nameAndAge);
+var Tallas;
+(function (Tallas) {
+    Tallas[Tallas["S"] = 0] = "S";
+    Tallas[Tallas["M"] = 1] = "M";
+    Tallas[Tallas["L"] = 2] = "L";
+    Tallas[Tallas["XL"] = 3] = "XL";
+})(Tallas || (Tallas = {}));
+var Tallas2;
+(function (Tallas2) {
+    Tallas2["Peque\u00F1a"] = "S";
+    Tallas2["Mediana"] = "M";
+    Tallas2["Grande"] = "L";
+    Tallas2["Extragrande"] = "XL";
+})(Tallas2 || (Tallas2 = {}));
+const talla = Tallas2.Pequeña;
+console.log(talla);
+const state = 2;
+console.log(state);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,56 @@ mensaje = "despues del breakpoint"
 console.log(mensaje, typeof mensaje)
 
 mensaje= "mensaje final"
+
+const arr = ["Array", "Hola"]
+
+console.log(arr, typeof arr)
+
+let age: number = 27
+
+let youngAdult: boolean = true
+
+const myName: string = "Lucía Martínez Cerón"
+
+console.log(typeof myName)
+
+const suma =(a: number, b: number) =>{
+return a + b
+}
+
+console.log(suma(4,5))
+
+let animal: string[] = ["caballito", "de", "mar"]
+let num: number[] = [33, 44, 55]
+let animales: Array<string> = ["caballito", "de", "mar"]
+let nums: Array<number> = [33, 44, 55]
+let num2 = [] // tipo any
+
+// nums.map(num => num.) el autocompletado sugiere métodos segun el tipo de dato
+
+
+num2= [44, 55]
+
+console.log(typeof num, typeof num2)
+
+let nameAndAge: [string, number] = ["Lucía", 27]
+
+let fullNameAndAge: [string[], number] = [["Lucía", "Martínez", "Cerón"], 27]
+
+nameAndAge.push(12) // nos lo añade después y nos destruye la dupla
+
+console.log(nameAndAge)
+
+enum Tallas {S, M, L, XL}
+
+enum Tallas2 {Pequeña = 'S', Mediana = 'M', Grande = 'L', Extragrande = 'XL'}
+
+const talla = Tallas2.Pequeña
+
+console.log(talla)
+
+const enum LoadingState {Idle, Loading, Success, Error}
+
+const state = LoadingState.Success
+
+console.log(state)
